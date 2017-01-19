1888ca57382c015986eddf4b6ba145568e2095d3
Mahmoud-Albasndi <mahmoud.albasndi@hotmail.com>
2017-01-19 13:45:23 +0200

    Delegate Type

---
+++ Delegate Type/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Type
+{
+    delegate int MyNewNumber(int MyNumber);
+    class Program
+    { 
+        static int MyXnumber = 3;
+        static int addFunct (int Number )
+        {
+            MyXnumber += Number;
+            return MyXnumber;
+        }
+        static int mulfunc(int number)
+        {
+            MyXnumber *= number;
+            return MyXnumber;
+        }
+        static void Main(string[] args)
+        {
+            MyNewNumber n1 = new MyNewNumber(addFunct);
+            MyNewNumber n2 = new MyNewNumber(mulfunc);
+            n1(5);
+            Console.Write($"{MyXnumber}\n");
+            n2(2);
+            Console.WriteLine($"{MyXnumber}");
+            Console.ReadKey();
+        }
+    }
+}
