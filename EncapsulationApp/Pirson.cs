2d636e0a962a7d4f3dfb0364377117b549f0ec2e
Mahmoud-Albasndi <mahmoud.albasndi@hotmail.com>
2017-01-19 13:43:56 +0200

    Encapsulation

---
+++ EncapsulationApp/Pirson.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationApp
+{
+    class Pirson
+    {
+        private string name;
+        private int age;
+        public void Printvalue() { Console.WriteLine($"Name is = {name} Age = {age}"); }
+
+        public Pirson() { }
+        public void pirson(string name , int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetAge (int Myage)
+        {
+            age = Myage;
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+
+
+
+    }
+}
---
+++ EncapsulationApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Mahmuoud Albasndi";
+            int age = 24;
+            Pirson MyObj = new Pirson();
+            MyObj.SetName(Name);
+            MyObj.SetAge(age);
+
+            Console.WriteLine("This is Name  " + MyObj.GetName());
+            Console.WriteLine("This is My Age  " + MyObj.GetAge());
+            Console.ReadKey();
+        }
+    }
+}
