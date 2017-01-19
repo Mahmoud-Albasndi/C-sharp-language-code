1c0b00352434bd9d3d0935e1353644daf5c4bd11
Mahmoud-Albasndi <mahmoud.albasndi@hotmail.com>
2017-01-19 13:48:10 +0200

    OverLoding

---
+++ OverLoding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoding
+{
+    class Program
+    {
+        
+        public static int minVal(int []MyAraay)
+        {
+            int Number = MyAraay[0];
+            for (int i = 1; i < MyAraay.Length; i++)
+            {
+                if (MyAraay[i] < Number)
+                     Number = MyAraay[i];
+            }    
+            return Number;
+        }
+        public static double minVal(double MyAraay)
+        {
+            return MyAraay;
+        } 
+
+        static void Main(string[] args)
+        {
+            int[] MyAraay = {19, 20, 30, 44, 5, 6, 7, 8, 9, 10};
+            Console.WriteLine($"The minumim Value is = {minVal(MyAraay)}");
+            Console.ReadKey();
+            
+        }
+    }
+}
