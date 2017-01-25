ea7e17a5da7a0bfab305ee8a7a22eafe9b1b1867
Mahmoud Albasndi <mahmoud.albasndi@hotmail.com>
2017-01-26 00:53:03 +0200

    Update Program.cs

--- Do While/Program.cs
+++ Do While/Program.cs
@@ -10,6 +10,19 @@ namespace Do_While
     {
         static void Main(string[] args)
         {
+            
+                   double mySalary = 1000.20, total = 0.0;
+            int i = 0;
+            do
+            {
+               total +=  mySalary;   //total = total  + mySalary;
+               i++;
+                Console.WriteLine("i values equal {0}",i);
+            } while (i < 10);
+            Console.WriteLine("Salary values equal {0}", total);
+
+          
+            
             //    int i = 0;
             //    do
             //    {
@@ -19,19 +32,7 @@ namespace Do_While
             //    } while (i < 5);
             //    Console.Write($"{i}");
 
-            int[][] MyArray = new int[2][];
-
-             MyArray[0] = new int [6]{ 7, 10, 12, 14, 18, 19 };
-             MyArray[1] = new int [6] { 1, 2, 4, 5, 8, 7 };
-            for (int i = 0; i < MyArray.Length; i++)
-            {
-                Console.WriteLine($"Elment{i}");
-
-                for (int j = 0; j < MyArray[i].Length; j++)
-                {
-                    Console.WriteLine("{0}" ,MyArray[i][j] + " ");
-                }
-                Console.WriteLine();
+            
             }
            Console.ReadKey();
         }
