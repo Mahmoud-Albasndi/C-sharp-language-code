ff050e9f0bbc9880fa535d713ded89db626496c9
Mahmoud-Albasndi <mahmoud.albasndi@hotmail.com>
2017-03-14 23:48:34 +0200

    do while

---
+++ Exception Filters/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Filters
+{
+    class Program
+    {
+        public static void salaryCal()
+        {
+            try
+            {
+
+                decimal var1 , tax, totaltax ,salary;
+                Console.WriteLine("Inter the total salary befot cut tax ");
+                var1 = Convert.ToDecimal(Console.ReadLine());
+                if (var1 < 501)
+                {
+                    throw new Exception("Salary Less than $500");
+                }
+                Console.WriteLine("Inter the tax");
+                tax = Convert.ToDecimal(Console.ReadLine());
+                if(tax == 0)
+                {
+                    throw new Exception("the must not be a zero");
+                }
+
+                totaltax = var1 * tax;
+                salary = var1 -totaltax;
+                Console.WriteLine("the total tax will be {0} and the salary will be {1}" ,totaltax, salary);
+            }
+            catch (FormatException ex)
+            {
+                throw new Exception("salary format or tax is not mathc", ex.InnerException);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                salaryCal();
+            }
+
+            catch (Exception MyEx) when (MyEx.Message.Equals("Salary Less than $500"))
+            {
+                Console.WriteLine("this has salary no taxs");
+            }
+            catch (Exception MyEx) when (MyEx.Message.Equals("the must not be a zero"))
+            {
+                Console.WriteLine("you entered a zero value for tax");
+            }
+            catch (Exception MyEx)
+            {
+                Console.WriteLine(MyEx.Message);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
--- Exception Filters/Properties/AssemblyInfo.cs
+++ Exception Filters/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Do While")]
+[assembly: AssemblyTitle("Exception Filters")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Do While")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("Exception Filters")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c869341c-0463-4f4f-843d-19572904661f")]
+[assembly: Guid("3919a50c-38cc-4570-988e-f7f6a24462e0")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
