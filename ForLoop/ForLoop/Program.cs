711405682fcaab148aefc9726a8a585974000f45
Mahmoud Albasndi <mahmoud.albasndi@hotmail.com>
2017-03-25 22:30:47 +0200

    Multiplication table

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    if (i == 6)
+                        continue;
+                    Console.WriteLine(i + "*" +j+ "=" + (i*j));
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
